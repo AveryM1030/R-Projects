b4fb1e2090308bdf66c93b110efa4958b749fe98
Avery Miles <MilesA@chc.edu>
2022-12-06 10:46:50 -0500

    December 6, 2022

--- describe.r
+++ describe.r
@@ -1,3 +1,6 @@
+# install the pacman contributed package
+install.packages("pacman")
+
 library()
 
 # the describe function is in a contributes package
@@ -7,3 +10,29 @@ library()
 # function
 # to install, load, and attach a contributed package
 # requires the pacman package
+require(pacman)
+
+# get R documentation on p_load
+?p_load
+
+#install , load, and attach psych using p_load
+p_load(psych)
+
+# get R documentation on on describe
+?describe
+
+# describe can be used with quantitative variables only
+# it gives more statistical information than the
+# summary function
+
+# call decsribe giving 1 quantitative variable
+describe(iris$Sepal.Length)
+
+# call describe giving it the iris data frame
+describe(iris)
+
+# detach and unload psych
+p_unload(psych)
+
+# detach and unload pacman
+detach(package:pacman)
